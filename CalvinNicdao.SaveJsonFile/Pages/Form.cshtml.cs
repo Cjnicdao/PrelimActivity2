2b78e7fff0175bb5450f8946d02582c3b0e3f9cd
KALVEN\user <cjnicdao@csmbataan.ph.education>
2022-09-17 08:35:15 +0800

    commit2

---
+++ CalvinNicdao.SaveJsonFile/Pages/Form.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalvinNicdao.SaveJsonFile_.Pages
+{
+    public class FormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
